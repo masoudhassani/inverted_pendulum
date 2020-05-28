a1f161d769a87546f63bb5571e4a8e9467ab38dd
Masoud Hassani <masoudh@cm-labs.com>
2020-05-28 15:49:31 -0400

    updated the training parameters and logic

--- vortex/environment.py
+++ vortex/environment.py
@@ -7,61 +7,80 @@ vortex_folder = r'C:\CM Labs\Vortex Studio 2020a\bin'
 sys.path.append(vortex_folder)
 import VxSim
 import vxatp
+import math
 
 class Environment:
     def __init__(self):
         self.config_file = r'E:\VTSP_Construction\content\resources\config\quality_tests_with_graphics.vxc'
         self.content_file = 'Mechanism.vxmechanism'
         self.episode = 0
-        self.motor_step = 0.0314159    # bipolar stepper motor step size is 1.8 deg 
+        self.motor_step = 0.0314159*10    # bipolar stepper motor step size is 1.8 deg 
         self.PI = 3.14159265359
         self.application = vxatp.VxATPConfig.createApplication(self, 'Inverted Pendulum', self.config_file)
         self.application.setSyncMode(VxSim.kSyncNone)   # set free running
         self.obj = self.application.getSimulationFileManager().loadObject(self.content_file)
 
         # discretized space size
-        NUM_DISCRETE = 30
-        self.num_states = 2
+        self.num_discrete = 60
+        self.num_states = 3
 
         # set up state limits
         if self.num_states == 4:
-            self.limits_low = np.array([-self.PI, -0.6*self.PI, -2*self.PI, -8*self.PI])
-            self.limits_high = np.array([self.PI, 0.6*self.PI, 2*self.PI, 8*self.PI])
+            self.limits_low = np.array([-self.PI, -0.6*self.PI, -2*self.PI, -10*self.PI])
+            self.limits_high = np.array([self.PI, 0.6*self.PI, 2*self.PI, 10*self.PI])
+        if self.num_states == 3:
+            self.limits_low = np.array([-6*self.PI, -2*self.PI, -10*self.PI])
+            self.limits_high = np.array([6*self.PI, 2*self.PI, 10*self.PI])            
         elif self.num_states == 2:
-            self.limits_low = np.array([-2*self.PI, -8*self.PI])
-            self.limits_high = np.array([2*self.PI, 8*self.PI])
+            self.limits_low = np.array([-2*self.PI, -10*self.PI])
+            self.limits_high = np.array([2*self.PI, 10*self.PI])
 
-        
+        # action and state space size
+        self.action_space_size = 3
+        self.state_space_size = [self.num_discrete] * self.num_states
+        self.encoded_state_size = self.num_discrete ** self.num_states
+
         # calculate discretization window
-        self.state_space_size = [NUM_DISCRETE] * self.num_states
         self.disc_state_win_size = [0] * self.num_states
         for i in range(self.num_states):
             self.disc_state_win_size[i] = (self.limits_high[i] - self.limits_low[i])/self.state_space_size[i]
 
-        # action size
-        self.action_space_size = 2
-
     ''' 
     reset the environment at the start of each episode
     '''
-    def reset(self):
+    def reset(self, real_time):
         self.mechanism = None
         self.interface = None
         self.motor_angle_current = 0    # this comes from the motor constraint  
         self.motor_angle_command = 0    # motor command, might be different from the actual motor angle
 
-        # unload the mechanism object if it exists
+        # go to edit mode
         vxatp.VxATPUtils.requestApplicationModeChangeAndWait(self.application, VxSim.kModeEditing)
+
+        # render realtime if requested otherwise free running
+        if real_time:
+            self.application.setSyncMode(VxSim.kSyncSoftwareAndVSync)
+        else:
+            self.application.setSyncMode(VxSim.kSyncNone)
+
+        # unload the mechanism object if it exists
         if self.obj:
-            self.application.getSimulationFileManager().unloadObject(self.obj)       
+            self.application.getSimulationFileManager().unloadObject(self.obj)   
+            self.obj = None    
 
         # recreate the mechanism object from the content file
         self.obj = self.application.getSimulationFileManager().loadObject(self.content_file)
-        vxatp.VxATPUtils.requestApplicationModeChangeAndWait(self.application, VxSim.kModeSimulating)
-
+        
         # reassign the mechanism variable
         self.mechanism = VxSim.MechanismInterface(self.obj)
         self.interface = self.mechanism.findExtensionByName('Agent Interface')
+
+        # reset the motor
+        self.interface.getExtension().getInput('Motor Position').value = 0       
+
+        # run 
+        vxatp.VxATPUtils.requestApplicationModeChangeAndWait(self.application, VxSim.kModeSimulating)
+
         self.application.update()
 
         # update hud
@@ -74,14 +93,13 @@ class Environment:
         print('--------------- environment was successfully reset --------------------')
         print('--------------------- starting a new episode--------------------')
 
-        # return the current state
-        return [0] * self.num_states
+        # return the current state, discrete and encoded
+        return [0] * self.num_states, 0
 
     '''
     step the simulation and return the state
     '''
     def step(self, action):
-        done = False
         self.reward = 0
         # decode the action
         # rotate motor one step left
@@ -89,9 +107,14 @@ class Environment:
             self.motor_angle_command -= self.motor_step
         
         # rotate motor one step right
-        else:
+        elif action == 1:
             self.motor_angle_command += self.motor_step
-        self.motor_angle_command = min(max(self.motor_angle_command, -self.PI), self.PI)
+        
+        # stop the motor
+        else:
+            pass
+
+        # self.motor_angle_command = min(max(self.motor_angle_command, -self.PI), self.PI)
 
         # push the motor command to the constraint
         self.interface.getExtension().getInput('Motor Position').value = self.motor_angle_command
@@ -99,30 +122,59 @@ class Environment:
         # start the simulation
         self.application.update()
 
-        # read current state
+        # read current state ###################################################
         motor_angle = self.interface.getExtension().getOutput('Motor Angle').getValue()
         motor_velocity = self.interface.getExtension().getOutput('Motor Angular Velocity').getValue()        
         pendulum_angle = self.interface.getExtension().getOutput('Pendulum Angle').getValue()
         pendulum_velocity = self.interface.getExtension().getOutput('Pendulum Angular Velocity').getValue()   
 
+        if pendulum_angle > 2*self.PI:
+            pendulum_angle = pendulum_angle - int(pendulum_angle/(2*self.PI))*2*self.PI
+        elif pendulum_angle < -2*self.PI:
+            pendulum_angle = pendulum_angle + int(-pendulum_angle/(2*self.PI))*2*self.PI
+
         if self.num_states == 4:
             state = np.array([motor_angle, motor_velocity, pendulum_angle, pendulum_velocity])
 
+        elif self.num_states == 3:
+            state = np.array([motor_velocity, pendulum_angle, pendulum_velocity])
+
         elif self.num_states == 2:     
             state = np.array([pendulum_angle, pendulum_velocity])
+        ##########################################################################
 
-        if pendulum_angle > 2*self.PI:
-            pendulum_angle = pendulum_angle - int(pendulum_angle/2*self.PI)*2*self.PI
-        elif pendulum_angle < -2*self.PI:
-            pendulum_angle = pendulum_angle + int(-pendulum_angle/2*self.PI)*2*self.PI
-
+        # discretize and encode the state ########################################
         # discretize the state
         disc_state = self.discretize_state(state)
 
-        # reward assignment
-        self.reward = -self.PI + abs(pendulum_angle)
+        # encode the state
+        enc_state = self.encode_state(disc_state)
+        ##########################################################################
 
-        return disc_state, self.reward
+        # reward assignment ######################################################
+        success_thresh = 10 * 0.0174533  # 10 deg
+        horizonal_threshold = 90 * 0.0174533  # 90 deg
+        if pendulum_angle >= 0:
+            diff = -1*abs(-self.PI + pendulum_angle)
+        else:
+            diff = -1*abs(self.PI + pendulum_angle)
+
+        # give positive reward if the pendulum is upward    
+        if -diff <= horizonal_threshold:
+            self.reward = 1
+        #     self.reward = (success_thresh + diff) * 5
+
+        # # give positive reward if the pendulum is horizontal   
+        # elif -diff <= horizonal_threshold:
+        #     self.reward = (horizonal_threshold + diff) * 1       
+
+        # # give negative reward if non of the above conditions are met
+        # else:
+        #     self.reward = (diff)    
+        #########################################################################
+    
+
+        return disc_state, enc_state, self.reward
 
     '''
     gets a continous state and returns a discretized state
@@ -132,9 +184,23 @@ class Environment:
         return list(discrete_state.astype(np.int8))  
 
     '''
+    gets a continous state and returns a encoded state
+    '''
+    def encode_state(self, state):
+        if len(state) == 2:
+            enc_state = state[0] * self.num_discrete + state[1]
+        elif len(state) == 3:
+            enc_state = state[0] * (self.num_discrete ** 2) + state[1] * self.num_discrete + state[2]
+        elif len(state) == 4:
+            enc_state = state[0] * (self.num_discrete ** 3) + state[1] * (self.num_discrete ** 2) + state[2] * self.num_discrete + state[3]
+
+        return enc_state
+
+    '''
     returns a random action
     0: rotate one step left
     1: rotate one step right
+    2: stop the motor
     '''
     def sample(self):
-        return random.choice([0, 1])
+        return random.choice([0, 1, 2])
--- vortex/training.py
+++ vortex/training.py
@@ -1,69 +1,97 @@
+from __future__ import print_function
 from environment import Environment
 import random
 import numpy as np
+import matplotlib.pyplot as plt 
+
 
 # parameters
 max_episodes = 10001
-max_steps = 600
+num_steps = 4800   # 80 sec
+num_steps_decay = 0.999
+min_num_steps = 600   # 10 sec
 epsilon = 1.0
-epsilon_decay = 0.999
+epsilon_decay = 0.998
 min_epsilon = 0.01
 discount_factor = 0.99
-learning_rate = 0.01
+learning_rate = 0.02
 learning_rate_decay = 0.99
 min_learning_rate = 0.001
 
-save_every = 2000
+save_every = 500
+print_every = 100
+render_real_time_every = 250
 
 # initialize the environment
 env = Environment()
 print(env.state_space_size)
 print(env.action_space_size)
 
+# initialization for plotting 
+# plt.ion()
+fig=plt.figure()
+episod_rewards = []
+
 # initialize the q table
-q_table_size = env.state_space_size
-q_table_size.append(env.action_space_size)
+q_table_size = (env.encoded_state_size, env.action_space_size)
 
 q_table = np.zeros(q_table_size)
-# q_table = np.random.uniform(low=-2, high=0, size=(env.state_space_size + [env.action_space_size]))
-print(q_table.shape)
+# q_table = np.random.uniform(low=-2300, high=-1500, size=q_table_size)
+print('q_table shape:', q_table.shape)
 
 for e in range(max_episodes):
-    current_state = env.reset()
+    # switch between real time and free running
+    real_time = False
+    if e % render_real_time_every == 0:
+        real_time = True
+
+    # reset the environment
+    _, current_state = env.reset(real_time)
+
+    # stabilizing the simulator at the satrt of each episode
+    for i in range(50):
+        _, current_state, _ = env.step(2)
+
     step = 0
     episod_reward = 0 
     reward = 0
-    done = False
-
-    while not done:
+    while True:
         step += 1
-        if step > max_steps:
+        if step > num_steps:
             break
 
         # epsilon greedy algorithm
         if np.random.random() > epsilon:
-            ind = np.argmax(q_table[current_state])
-            action = ind - int(ind/env.action_space_size)*env.action_space_size
-            
-            # print(np.max(q_table[current_state]))
-            # print(q_table[current_state])
-            # print(current_state)
-            # print(ind)
-            # print(action)
+            action = np.argmax(q_table[current_state])
+            if e % print_every == 0:
+                print('agent action:', action, end='\r')
 
         else:
             action = env.sample()
+            # if e % print_every == 0:
+                # print('randm action:', action)
 
-        # update q_table
-        new_state, reward = env.step(action)
+        # take ac action and read the consequent state
+        _, new_state, reward = env.step(action)
+
+        # max possible q value of the next state
         max_future_q = np.max(q_table[new_state])
-        current_q = q_table[current_state + [action, ]]
+
+        # q value of the state/action combination
+        current_q = q_table[current_state][action]
+
+        # update the q value for state/action combination
         new_q = (1 - learning_rate) * current_q + learning_rate * (reward + discount_factor * max_future_q)
-        q_table[current_state + [action, ]] = new_q
+        q_table[current_state][action] = new_q
+
+        # set the captured state as the current state
         current_state = new_state
 
         # update episod reward
         episod_reward += reward
+    
+    # add the cuurent episode reward to plot
+    episod_rewards.append(episod_reward)
 
     # update epsilon
     epsilon = max(min_epsilon, epsilon*epsilon_decay)
@@ -71,10 +99,15 @@ for e in range(max_episodes):
     # update learning rate
     learning_rate = max(min_learning_rate, learning_rate*learning_rate_decay)
 
+    # update number of steps
+    num_steps = max(min_num_steps, int(num_steps*num_steps_decay))
 
     if e % save_every == 0:
         np.save('q_table_ep{}.npy'.format(e), q_table)
         
-    print('episode: ', e, 'epsilon:', epsilon, 'learning_rate:', learning_rate, 'episod_reward:', episod_reward)
+    print('episode: ', e, 'epsilon:', epsilon, 'learning_rate:', learning_rate, 'max steps:', num_steps, 'episod_reward:', episod_reward)
 
 print(q_table)
+plt.plot(episod_rewards)
+plt.draw() 
+plt.pause(5)
